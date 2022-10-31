6a360ac48cb277a5fe3ac3a5bdf61803cd19008b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-31 10:49:26 -0400

    added Home content component and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // common components
 import Header from './Views/components/header';
 import Footer from './Views/components/footer';
+
+// content components
+import Home from './Views/content/home';
 
 // Styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-
-
-      <h1 className="container">Hello, World!</h1>
-
-      <Footer></Footer>
+      <BrowserRouter>
+        <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+          </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
