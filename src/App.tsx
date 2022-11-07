115a051a25ad588b6de918762b483b62a9e34c44
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-07 08:26:07 -0500

    added the remaining routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,22 @@ import Footer from './Views/components/footer';
 
 // content components
 import Home from './Views/content/home';
+import About from './Views/content/about';
+import Projects from './Views/content/projects';
+import Services from './Views/content/services';
+import Contact from './Views/content/contact';
 
 // Styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
+// JavaScript Libraries
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,6 +30,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/home" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/services" element={<Services />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
           </Routes>
         <Footer></Footer>
       </BrowserRouter>
