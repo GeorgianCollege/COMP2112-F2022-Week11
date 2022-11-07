d475b8c0141e293de313f406bf86ef200d765f31
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-07 10:51:20 -0500

    updated Contact form with some events

--- src/Views/content/contact.tsx
+++ src/Views/content/contact.tsx
@@ -1,10 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
 function Contact()
 {
+    const [fullName, setFullName] = useState('');
+    const [contactNumber, setContactNumber] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [message, setMessage] = useState('');
+
     useEffect(() =>{
         document.title = "Contact";
     },[]);
+
+    function onChangeFullName(e: ChangeEvent<HTMLInputElement>)
+    {
+        console.log(fullName);
+    }
+
+    function onChangeContactNumber(e: ChangeEvent<HTMLInputElement>)
+    {
+        console.log(contactNumber);
+    }
+
+    function onChangeEmailAddress(e: ChangeEvent<HTMLInputElement>)
+    {
+        console.log(emailAddress);
+    }
+
+    function onChangeMessage(e: ChangeEvent<HTMLTextAreaElement>)
+    {
+        console.log(message);
+    }
 
     return(
         <div className="container">
@@ -17,22 +42,25 @@ function Contact()
 
                     <div className="form-group mb-3">
                         <label htmlFor="fullName">Full Name</label>
-                        <input type="text" className="form-control" id="fullName" name="fullName" aria-describedby="Full Name Input" placeholder="Enter Your Full Name" required/>
+                        <input type="text" className="form-control" id="fullName" name="fullName" onChange={onChangeFullName} 
+                        aria-describedby="Full Name Input" placeholder="Enter Your Full Name" required/>
                     </div>
 
                     <div className="form-group mb-3">
                         <label htmlFor="contactNumber">Contact Number</label>
-                        <input type="tel" className="form-control" id="contactNumber" name="contactNumber" aria-describedby="Contact Number Input" placeholder="Enter Your Contact Number" required/>
+                        <input type="tel" className="form-control" id="contactNumber" name="contactNumber" onChange={onChangeContactNumber} 
+                        aria-describedby="Contact Number Input" placeholder="Enter Your Contact Number" required/>
                     </div>
 
                     <div className="form-group mb-3">
                         <label htmlFor="emailAddress">Email address</label>
-                        <input type="email" className="form-control" id="emailAddress" name="emailAddress" aria-describedby="Email Address Input" placeholder="Enter Your Email Address" required/>
+                        <input type="email" className="form-control" id="emailAddress" name="emailAddress" onChange={onChangeEmailAddress} 
+                        aria-describedby="Email Address Input" placeholder="Enter Your Email Address" required/>
                     </div>
                     
                     <div className="form-group mb-3">
                         <label htmlFor="message">Your Message</label>
-                        <textarea className="form-control" id="message" name="message"aria-label="Message Area"></textarea>
+                        <textarea className="form-control" id="message" name="message"aria-label="Message Area" onChange={onChangeMessage}></textarea>
                     </div>
 
                     <button type="submit" id="submitButton" className="btn btn-primary me-2"><i className="fas fa-envelope fa-lg"></i> Submit</button>
